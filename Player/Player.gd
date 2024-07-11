81d6a9c402cbd481ec69c3d30de0a543f3bdf9e2
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-07-12 00:34:41 +0300

    shoot system.update

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,12 +3,22 @@ extends CharacterBody2D
 
 const SPEED = 100
 
+var bullet = preload("res://bullet/area_2d.tscn")
 
 func get_input():
 	var input_direction = Input.get_vector("LEFT","RIGHT","UP","DOWN")
 	velocity = input_direction * SPEED
 	look_at(get_global_mouse_position())
+
+func shoot():
+	var b = bullet.instantiate()
+	owner.add_child(b)
+	b.transform = $GunCast2D/Marker2D.global_transform
 	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	$GunCast2D.look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("SHOOT"):
+		shoot() 
+		
