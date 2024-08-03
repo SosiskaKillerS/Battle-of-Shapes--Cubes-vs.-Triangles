d10aa30c4bb3b9e9f3464a2b88ffac984eff589d
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-08-03 12:16:56 +0300

    spawn system ready

--- Spawn/spawn_scene.gd
+++ Spawn/spawn_scene.gd
@@ -1,18 +1,10 @@
 extends Node2D
 
-@onready var enemy_scene = load("res://mobs/enemy_1.tscn") 
-@onready var spawn_scene = $SpawnPoint 
+@onready var enemy_scene = preload("res://mobs/enemy_1.tscn")
 
-func spawn_enemy():
+
+
+func _on_timer_timeout():
 	var enemy = enemy_scene.instantiate()
-	add_child(enemy)
-
-func _ready():
-	set_process(true)
-	set_physics_process(true)
-	spawn_enemy()
-	
-
-func _process(delta):
-	if randf() < delta * 0.5:
-		spawn_enemy()
+	enemy.position = position
+	get_parent().get_node(".").add_child(enemy)
